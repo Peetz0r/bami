fb4b25db4a1079484fdf5437d80163f01fe2f86d
Peetz0r <peter@haas-en-berg.nl>
2024-03-23 10:57:12 +0100

    flutter create

---
+++ README.md
@@ -0,0 +1,3 @@
+# BAMI: Bambu Advanced Monitoring Interface
+
+An app to monitor and control your Bambu Labs 3D printer in LAN mode.
---
+++ android/app/src/main/kotlin/nl/peetz0r/bami/MainActivity.kt
@@ -0,0 +1,5 @@
+package nl.peetz0r.bami
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
