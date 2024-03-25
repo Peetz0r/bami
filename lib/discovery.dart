25e8989400b552e9daaa7565263f34cf2a3c3b0b
Peetz0r <peter@haas-en-berg.nl>
2024-03-25 01:21:29 +0100

    Improve settings dialog style and layout

--- lib/discovery.dart
+++ lib/discovery.dart
@@ -162,93 +162,143 @@ class _DiscoveryPageState extends State<DiscoveryPage> {
     if (_testState == 3) return Icon(Icons.report, size: 20, color: Colors.red);
   }
 
-  AlertDialog _settingsDialog(Bambu printer, setState, [bool connect = false]) {
+  Dialog _settingsDialog(Bambu printer, setState, [bool connect = false]) {
 
-    return AlertDialog(
-      title: const Text('Settings'),
-      content: SingleChildScrollView(
+    return Dialog(
+      clipBehavior: Clip.hardEdge,
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+        constraints: BoxConstraints.tightFor(width: 420),
         child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: Card(
-                child: ListTile(
-                  leading: FlutterLogo(size: 72.0),
-                  title: Text(printer.name),
-                  subtitle: Text('Model: ${printer.model}\nIP: ${printer.ip}'),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 12.0),
+              alignment: Alignment.centerLeft,
+              child: const Text(
+                'Settings',
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w400,
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Access Code',
-                  suffix: _testDisplay(),
-                ),
-                initialValue: _tmpPass,
-                enableSuggestions: false,
-                keyboardType: TextInputType.visiblePassword,
-                onChanged: (String text) {
-                  _tmpPass = text;
-                  if (_tmpPass.length == 8) {
-                    setState(() { _testState = 1; });
-                    printer.testConnection(_tmpPass).then((e) {
-                      setState(() { _testState = e ? 2 : 3; });
-                    });
-                  } else {
-                    setState(() { _testState = 0; });
-                  }
-                },
-              ),
-            ),
-            SwitchListTile(
-              title: const Text("Auto-connect"),
-              subtitle: const Text("Automatically connect to this printer once detected, skipping the device list"),
-              value: _tmpAutoConnect,
-              onChanged: (bool value) {
-                print("autoConnect: $value");
-                setState(() { _tmpAutoConnect = value; });
-              },
-            ),
-            Card(
-              child: Padding(
-                padding: EdgeInsets.all(12.0),
-                child: Column(
-                  children: [
-                    Text("You can only connect locally to a Bambu Labs 3D printer in LAN Only mode. To do this you'll need to enable LAN Only mode on the screen on your printer, and enter the Access Code from the printers display here."),
-                    Padding(
-                      padding: EdgeInsets.all(4.0),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          launchUrl(Uri.parse('https://wiki.bambulab.com/en/knowledge-sharing/enable-lan-mode'));
-                        },
-                        child: Text("More details on Bambu Lab Wiki"),
+            Flexible(
+              child: Scrollbar(
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Card(
+                          child: ListTile(
+                            leading: FlutterLogo(size: 72.0),
+                            title: Text(printer.name),
+                            subtitle: Text('Model: ${printer.model}\nIP: ${printer.ip}'),
+                          ),
+                        ),
                       ),
-                    ),
-                  ],
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Access Code',
+                            suffix: _testDisplay(),
+                          ),
+                          initialValue: _tmpPass,
+                          enableSuggestions: false,
+                          keyboardType: TextInputType.visiblePassword,
+                          onChanged: (String text) {
+                            _tmpPass = text;
+                            if (_tmpPass.length == 8) {
+                              setState(() { _testState = 1; });
+                              printer.testConnection(_tmpPass).then((e) {
+                                setState(() { _testState = e ? 2 : 3; });
+                              });
+                            } else {
+                              setState(() { _testState = 0; });
+                            }
+                          },
+                        ),
+                      ),
+                      SwitchListTile(
+                        title: const Text("Auto-connect"),
+                        subtitle: const Text("Automatically connect to this printer once detected, skipping the device list"),
+                        value: _tmpAutoConnect,
+                        onChanged: (bool value) {
+                          print("autoConnect: $value");
+                          setState(() { _tmpAutoConnect = value; });
+                        },
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Card(
+                          child: Padding(
+                            padding: EdgeInsets.all(12.0),
+                            child: Column(
+                              children: [
+                                Text("You can only connect locally to a Bambu Labs 3D printer in LAN Only mode. To do this you'll need to enable LAN Only mode on the screen on your printer, and enter the Access Code from the printers display here."),
+                                Text("You can only connect locally to a Bambu Labs 3D printer in LAN Only mode. To do this you'll need to enable LAN Only mode on the screen on your printer, and enter the Access Code from the printers display here."),
+                                Text("You can only connect locally to a Bambu Labs 3D printer in LAN Only mode. To do this you'll need to enable LAN Only mode on the screen on your printer, and enter the Access Code from the printers display here."),
+                                Text("You can only connect locally to a Bambu Labs 3D printer in LAN Only mode. To do this you'll need to enable LAN Only mode on the screen on your printer, and enter the Access Code from the printers display here."),
+                                Text("You can only connect locally to a Bambu Labs 3D printer in LAN Only mode. To do this you'll need to enable LAN Only mode on the screen on your printer, and enter the Access Code from the printers display here."),
+                                Text("You can only connect locally to a Bambu Labs 3D printer in LAN Only mode. To do this you'll need to enable LAN Only mode on the screen on your printer, and enter the Access Code from the printers display here."),
+                                Text("You can only connect locally to a Bambu Labs 3D printer in LAN Only mode. To do this you'll need to enable LAN Only mode on the screen on your printer, and enter the Access Code from the printers display here."),
+                                Text("You can only connect locally to a Bambu Labs 3D printer in LAN Only mode. To do this you'll need to enable LAN Only mode on the screen on your printer, and enter the Access Code from the printers display here."),
+                                Text("You can only connect locally to a Bambu Labs 3D printer in LAN Only mode. To do this you'll need to enable LAN Only mode on the screen on your printer, and enter the Access Code from the printers display here."),
+                                Text("You can only connect locally to a Bambu Labs 3D printer in LAN Only mode. To do this you'll need to enable LAN Only mode on the screen on your printer, and enter the Access Code from the printers display here."),
+                                Text("You can only connect locally to a Bambu Labs 3D printer in LAN Only mode. To do this you'll need to enable LAN Only mode on the screen on your printer, and enter the Access Code from the printers display here."),
+                                Text("You can only connect locally to a Bambu Labs 3D printer in LAN Only mode. To do this you'll need to enable LAN Only mode on the screen on your printer, and enter the Access Code from the printers display here."),
+                                Text("You can only connect locally to a Bambu Labs 3D printer in LAN Only mode. To do this you'll need to enable LAN Only mode on the screen on your printer, and enter the Access Code from the printers display here."),
+                                Text("You can only connect locally to a Bambu Labs 3D printer in LAN Only mode. To do this you'll need to enable LAN Only mode on the screen on your printer, and enter the Access Code from the printers display here."),
+                                Padding(
+                                  padding: EdgeInsets.all(4.0),
+                                  child: ElevatedButton(
+                                    onPressed: () {
+                                      launchUrl(Uri.parse('https://wiki.bambulab.com/en/knowledge-sharing/enable-lan-mode'));
+                                    },
+                                    child: Text("More details on Bambu Lab Wiki"),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 4.0),
+              child: Row (
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  TextButton(
+                    child: const Text('Cancel'),
+                    onPressed: Navigator.of(context).pop,
+
+                  ),
+                  ElevatedButton(
+                    child: Text(connect ? 'Connect' : 'Save'),
+                    onPressed: ((_testState == 0 && _tmpPass.length == 8) || _testState == 2) ? () {
+                      printer.save(_tmpPass, _tmpAutoConnect);
+                      Navigator.of(context).pop();
+                      if (connect) printer.connect(context);
+                    } : null,
+                  ),
+                ],
               ),
             ),
           ],
         ),
       ),
-      actions: <Widget>[
-        TextButton(
-          child: const Text('Cancel'),
-          onPressed: Navigator.of(context).pop,
-
-        ),
-        ElevatedButton(
-          child: Text(connect ? 'Connect' : 'Save'),
-          onPressed: ((_testState == 0 && _tmpPass.length == 8) || _testState == 2) ? () {
-            printer.save(_tmpPass, _tmpAutoConnect);
-            Navigator.of(context).pop();
-            if (connect) printer.connect(context);
-          } : null,
-        ),
-      ],
     );
   }
 
